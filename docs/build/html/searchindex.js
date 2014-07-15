a7d8121a0475042143f7a8209164282eaca99f09
Peng Miao <mios@PengtekiMacBook-Pro.local>
2014-07-15 15:26:22 +0800

    first commit for docs

---
+++ docs/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:42,terms:{index:0,search:0,modul:0,hello:0,content:0,world:0,page:0},objtypes:{},objnames:{},filenames:["index"],titles:["Welcome to Yottaa F2E Docs&#8217;s documentation!"],objects:{},titleterms:{welcom:0,f2e:0,doc:0,indic:0,tabl:0,yottaa:0,document:0}})
